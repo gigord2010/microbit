a749c95b5f4ea22f183d7d0e18459b5358319882
gigord2010 <38643218+gigord2010@users.noreply.github.com>
2018-04-24 17:31:22 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-﻿# sensors
+# sensors
 
 這是專為micro:bit提供的感應器積木
 
@@ -18,5 +18,5 @@ https://github.com/MonadnockSystems/pxt-dht11
 (The metadata above is needed for package search.)
 
 ```package
-sensors=github:gigord2010/microbit
+sensors=github:lioujj/pxt-sensors
 ```
--- main.ts
+++ main.ts
@@ -62,11 +62,11 @@ namespace sensors {
         MicroSeconds
     }
 
-    //% blockId=sensor_ping block=" ultrasonic trig %trig|echo %echo|get distance %unit"
-    //% trig.fieldEditor="gridpicker" trig.fieldOptions.columns=5
-    //% trig.fieldOptions.tooltips="false" trig.fieldOptions.width="350"
-    //% echo.fieldEditor="gridpicker" echo.fieldOptions.columns=5
-    //% echo.fieldOptions.tooltips="false" echo.fieldOptions.width="350"
+    //% blockId=sensor_ping block="ultrasonic trig %trig|echo %echo|get distance %unit"
+    //% trig.fieldEditor="gridpicker" trig.fieldOptions.columns=4
+    //% trig.fieldOptions.tooltips="false" trig.fieldOptions.width="300"
+    //% echo.fieldEditor="gridpicker" echo.fieldOptions.columns=4
+    //% echo.fieldOptions.tooltips="false" echo.fieldOptions.width="300"
     export function sensor_ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
         pins.setPull(trig, PinPullMode.PullNone);
@@ -98,4 +98,18 @@ namespace sensors {
         pins.analogWritePin(GreenPin, pins.map(GreenValue, 0, 255, 0, 1023));
         pins.analogWritePin(BluePin, pins.map(BlueValue, 0, 255, 0, 1023));
     }
+	//% blockId=DDMmotor block="Mcontrol pin %McontrolPin|speed of Mcontrol(0~1) %McontrolValue|MSpeed pin %MSpeedPin|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
+    //% McontrolValue.min=0 McontrolValue.max=1 MSpeedValue.min=0 MSpeed.max=255
+    //% McontrolPin.fieldEditor="gridpicker" McontrolPin.fieldOptions.columns=4
+    //% McontrolPin.fieldOptions.tooltips="false" McontrolPin.fieldOptions.width="300"
+	//% MSpeedPin.fieldEditor="gridpicker" MSpeedPin.fieldOptions.columns=4
+    //% MSpeedPin.fieldOptions.tooltips="false" MSpeedPin.fieldOptions.width="300"
+    
+    export function DDMmotor(McontrolPin: AnalogPin,McontrolValue: number,MSpeedPin: AnalogPin,MSpeedValue: number): void {
+        pins.analogWritePin(McontrolPin, pins.map(McontrolValue, 0, 1, 0, 1023));
+        pins.analogWritePin(MSpeedPin, pins.map(MSpeedValue, 0, 255, 0, 1023));
+	   
+
+    }
+	
 }
